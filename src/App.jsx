598c727f9f6f0961aba6329ed87029dc68e96b48
felipeluis223 <lf9072962@gmail.com>
2024-10-01 18:57:20 -0300

    feat: aplicando autenticação - mockado(rotas)

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 export default function App() {
 
+  // Aplicando autenticação de login - Mockado
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <Routes>
         
         <Route path="" element={<p>login</p>} />
-        <Route path="home" element={<p>home</p>} />
-        <Route path="stock" element={<p>stock</p>} />
-        <Route path="sales" element={<p>sales</p>} />
-        <Route path="clients" element={<p>clients</p>} />
-        <Route path="profile" element={<p>profile</p>} />
+
+        <Route path="home" element={isAuthenticated ? <p>home</p> : <Navigate to="/" />} >
+          <Route path="stock" element={<p>stock</p>} />
+          <Route path="sales" element={<p>sales</p>} />
+          <Route path="clients" element={<p>clients</p>} />
+          <Route path="profile" element={<p>profile</p>} />
+        </Route>
+
+        <Route path="*" element={<p>Not Found - 404</p>} />
 
       </Routes>
     </Router>
