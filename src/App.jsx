acb9d2ff5ec48bf0208aa5d238b5cfe37b5f5966
felipeluis223 <lf9072962@gmail.com>
2024-10-01 18:46:52 -0300

    feat: Criação das rotas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
 export default function App() {
+
   return (
-    <p>Hello world</p>
+    <Router>
+      <Routes>
+        
+        <Route path="" element={<p>login</p>} />
+        <Route path="home" element={<p>home</p>} />
+        <Route path="stock" element={<p>stock</p>} />
+        <Route path="sales" element={<p>sales</p>} />
+        <Route path="clients" element={<p>clients</p>} />
+        <Route path="profile" element={<p>profile</p>} />
+
+      </Routes>
+    </Router>
   )
 }
