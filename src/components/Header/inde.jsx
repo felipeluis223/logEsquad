6b8c2b9046869633c80b5350c38a2e587f03d974
felipeluis223 <lf9072962@gmail.com>
2024-10-07 19:33:16 -0300

    feat: aplicando o icone de perfil

--- src/components/Header/inde.jsx
+++ src/components/Header/inde.jsx
@@ -1,3 +1,4 @@
+import { IoPersonCircle } from "react-icons/io5";
 import { FaHome, FaBox, FaFolderPlus, FaIdCard } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
@@ -18,7 +19,10 @@ const Button = ({ Icon, description, route }) => {
 export default function Header({ onLogout }) {
     return (
         <section className="w-full bg-[#242424]">
-            <h3 className="text-[#1dd05d] py-[25px] text-xl font-bold italic text-center">LogEsquad</h3>
+            <div className="flex flex-row justify-between items-center px-[40px]">
+                <h3 className="text-[#1dd05d] py-[25px] text-xl font-bold italic">LogEsquad</h3>
+                <button className="flex flex-row gap-[10px] text-[#f1f1f1] items-center hover:text-[#1dd05d]"><IoPersonCircle className="text-3xl" /> Luis Felipe</button>
+            </div>
             <div className="w-full">
                 <ul className="h-full flex flex-row justify-evenly items-center flex-wrap">
                     <li><Button Icon={FaHome} description="Início" route="" /></li>
@@ -27,7 +31,8 @@ export default function Header({ onLogout }) {
                     <li><Button Icon={FaFolderPlus} description="Vendas" route="sales" /></li>
                 </ul>
             </div>
-            {/* <button className="bg-[yellow]" onClick={onLogout}>Sair</button> */}
         </section>
     );
 }
+
+// <button className="bg-[#1f1f1f] w-[80px] h-[50px] roundend-md" onClick={onLogout}>Sair</button>
